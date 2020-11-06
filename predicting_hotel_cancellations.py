72531a9e5ede742c4c74b7cf0cd849a12f567ec1
merrillm1 <58719008+merrillm1@users.noreply.github.com>
2020-11-05 21:21:39 -0800

    added function details

--- predicting_hotel_cancellations.py
+++ predicting_hotel_cancellations.py
@@ -131,10 +131,9 @@ filtered_plot = func.timeseries_frequency_plot(
 
 adjusted_loss = func.daily_loss(final)
 
-st.write("Average daily projected loss: ")
-st.write(round(adjusted_loss, 2))
-
 if st.checkbox('Show the impact on revenue?'):
 	st.subheader('Based on your adjustments, here are the results:')
-	st.pyplot(filtered_plot)   
+	st.pyplot(filtered_plot)
+	st.write("Average daily projected loss: ")
+	st.write(round(adjusted_loss, 2))   
 
